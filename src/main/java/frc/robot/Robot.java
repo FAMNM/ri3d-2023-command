3c3bb80b08adf36ad0d342220745e5ba11387b63
Krishna Dihora <kjdih2@gmail.com>
2023-01-07 15:20:42 -0500

    Added new triggers for buttons

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -15,9 +16,11 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
 
+  private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
+
+  UsbCamera camera;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -28,6 +31,9 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+    // Vision stuff here if we ant
+
   }
 
   /**
@@ -44,6 +50,7 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+    
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,8 +8,10 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
@@ -22,14 +24,49 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
+  // private CommandXboxController
+
+  private final XboxController driver = new XboxController(0);
+  private final XboxController secondary = new XboxController(1);
+
+  // Driver button triggers
+  private final Trigger driverA = new JoystickButton(driver, XboxController.Button.kA.value);
+  private final Trigger driverB = new JoystickButton(driver, XboxController.Button.kB.value);
+  private final Trigger driverX = new JoystickButton(driver, XboxController.Button.kX.value);
+  private final Trigger driverY = new JoystickButton(driver, XboxController.Button.kY.value);
+  private final Trigger driverLB = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
+  private final Trigger driverRB = new JoystickButton(driver, XboxController.Button.kRightBumper.value);
+  private final Trigger driverStart = new JoystickButton(driver, XboxController.Button.kStart.value);
+  private final Trigger driverBack = new JoystickButton(driver, XboxController.Button.kBack.value);
+  private final Trigger driverJoystickLeft = new JoystickButton(driver, XboxController.Button.kLeftStick.value);
+  private final Trigger driverJoystickRight = new JoystickButton(driver, XboxController.Button.kRightStick.value);
+  
+  // Secondary controller button triggers
+  private final Trigger secondaryA = new JoystickButton(secondary, XboxController.Button.kA.value);
+  private final Trigger secondaryB = new JoystickButton(secondary, XboxController.Button.kB.value);
+  private final Trigger secondaryX = new JoystickButton(secondary, XboxController.Button.kX.value);
+  private final Trigger secondaryY = new JoystickButton(secondary, XboxController.Button.kY.value);
+  private final Trigger secondaryLB = new JoystickButton(secondary, XboxController.Button.kLeftBumper.value);
+  private final Trigger secondaryRB = new JoystickButton(secondary, XboxController.Button.kRightBumper.value);
+  private final Trigger secondaryStart = new JoystickButton(secondary, XboxController.Button.kStart.value);
+  private final Trigger secondaryBack = new JoystickButton(secondary, XboxController.Button.kBack.value);
+  private final Trigger secondaryJoystickLeft = new JoystickButton(secondary, XboxController.Button.kLeftStick.value);
+  private final Trigger secondaryJoystickRight = new JoystickButton(secondary, XboxController.Button.kRightStick.value);
+
+  
+
+
+
+
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  // private final CommandXboxController m_driverController =
+  //     new CommandXboxController(OperatorConstants.kDriverControllerPort);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
     configureBindings();
+
   }
 
   /**
@@ -42,13 +79,19 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
+
+
+
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+    // new Trigger(m_exampleSubsystem::exampleCondition)
+    //     .onTrue(new ExampleCommand(m_exampleSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+
+
+
   }
 
   /**
